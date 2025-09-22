c3f62186d30c012d98848e9d2c739a47b2d16b47
so0orz <sorooshmazloomi@gmail.com>
2025-09-22 12:30:51 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./css/main.css";
 import "./css/custom.css";
+import logo from "./assets/icons/svgs/logo.svg";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
